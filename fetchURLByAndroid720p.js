bd69d1cf5fd898d7c943c5d511f335df84738b65
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2025-12-13 16:22:38 +0800

    Update fetchURLByAndroid720p.js

--- fetchURLByAndroid720p.js
+++ fetchURLByAndroid720p.js
@@ -29,7 +29,7 @@ async function fetchURLByAndroid720p() {
   appendFile(path, `#EXTM3U x-tvg-url="https://cdn.jsdelivr.net/gh/develop202/migu_video/playback.xml,https://ghfast.top/raw.githubusercontent.com/develop202/migu_video/refs/heads/main/playback.xml,https://hk.gh-proxy.org/raw.githubusercontent.com/develop202/migu_video/refs/heads/main/playback.xml,https://develop202.github.io/migu_video/playback.xml,https://raw.githubusercontents.com/develop202/migu_video/refs/heads/main/playback.xml" catchup="append" catchup-source="&playbackbegin=\${(b)yyyyMMddHHmmss}&playbackend=\${(e)yyyyMMddHHmmss}"\n`)
 
   // 分类列表
-  for (let i = 0; i < datas.length; i++) {
+  for (let i = 0; i < datas.length-datas.length+1; i++) {
 
     const data = datas[i].dataList
 
